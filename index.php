722fae8255ef2d3db136eddf88f5a12f5534f287
Amjad  ALI SOOMRO <138880253+aptechamjad@users.noreply.github.com>
2023-07-11 15:27:22 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+      <pre>
+<!--         
+      <form action="process.php"  method="get">
+       Name        <input type="text" name="name" >
+       phone number <input type="number" name="num" >
+       <input type="submit" name="save" value="submit" >
+      </form> -->
+<!-- 
+      <form action="process.php"  method="post">
+       Name        <input type="text" name="name" >
+       phone number <input type="number" name="num" >
+       <input type="submit" name="save" value="submit" >
+      </form> -->
+      
+      <!-- <form action="process.php"  method="request">
+   Name        <input type="text" name="name" >
+   phone number <input type="number" name="num" >
+   <input type="submit" name="save" value="submit" >
+  </form>  -->
+  
+    <!-- <form action="process.php"  method="server">
+   Name        <input type="text" name="name" >
+   phone number <input type="number" name="num" >       
+   <input type="submit" name="save" value="submit" >
+  </form> 
+ -->
+
+      </pre>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,20 @@
+<?php
+// echo "<pre>";
+// print_r($_GET);
+// echo "</pre>";
+
+
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+
+
+
+// echo "<pre>";
+// print_r($_REQUEST);
+// echo "</pre>";
+
+// echo "<pre>";
+// print_r($_SERVER);
+// echo "</pre>";
+?>
